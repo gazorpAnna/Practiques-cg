6a85f7223a198b3b705771db7adddd6a35565636
Anna Garcia <anna.gmoreno95@gmail.com>
2017-09-24 21:39:23 +0200

    Add files via upload

--- practica1Main.m
+++ practica1Main.m
@@ -1,3 +1,19 @@
-n=[3 21];
-d=[1 2 10];
-[z,p,k]=tf2zp(n,d);
+%% Calculation of poles and zeros
+
+n=[3 21];   % numerador   
+d=[1 2 10]; % denominador
+
+[z,p,k]=tf2zp(n,d); % troba els pols, zeros i guany d'una funcio de transferencia
+% z = zeros, p = poles, k = gain 
+[b,a]=zp2tf(z,p,k); % a partir dels pols, zeros i guany, troba la funcio de transferencia
+% b = numerador, a = denominador
+
+%% Decomposition in simple fractions
+
+n=[1 6];
+d=[1 6 8 0];
+
+[r,p]=residue(n,d); % calcula la descomposicio en fraccions simples
+
+%% Response to a step entry Matlab
+
